0822cc0d4dfa8ff2eef57922f1569b70fb813cde
VALIDEXIS <validexisglobal@gmail.com>
2025-06-12 09:17:14 +0300

    Update installmain.sh

--- installmain.sh
+++ installmain.sh
@@ -35,7 +35,7 @@ cd $HOME
 rm -rf gaia
 git clone https://github.com/cosmos/gaia.git
 cd gaia
-git checkout v23.3.0
+git checkout v24.0.0
 make install
 
 gaiad config set client chain-id cosmoshub-4
