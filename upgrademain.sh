c74555b5afba5b143f7943d682d93000a1132259
VALIDEXIS <validexisglobal@gmail.com>
2025-06-12 09:17:31 +0300

    Update upgrademain.sh

--- upgrademain.sh
+++ upgrademain.sh
@@ -3,6 +3,6 @@ cd $HOME
 rm -rf gaia
 git clone https://github.com/cosmos/gaia.git
 cd gaia
-git checkout v23.3.0
+git checkout v24.0.0
 make install
 ​sudo systemctl restart gaiad && sudo journalctl -u gaiad -f --no-hostname -o cat
